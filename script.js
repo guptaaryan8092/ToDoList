8d21cebd1582e2f44f51e1705e6924ce08683d34
guptaaryan8092 <guptaaryan8092@gmail.com>
2024-12-16 01:36:44 +0530

    dark mode added

--- script.js
+++ script.js
@@ -3,8 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const addTaskButton = document.getElementById('add-task-btn');
   const todoList = document.getElementById('todo-list');
   const dueDateInput = document.getElementById('due-date-input');
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  const darkModeIcon = darkModeToggle.querySelector('i');
 
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode')) || false;
+
+  if (isDarkMode) {
+    document.body.classList.add('dark-mode');
+    darkModeIcon.classList.add('fa-sun');
+    darkModeIcon.classList.remove('fa-moon');
+  } else {
+    document.body.classList.add('light-mode');
+    darkModeIcon.classList.add('fa-moon');
+    darkModeIcon.classList.remove('fa-sun');
+  }
+
+  darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+    if (document.body.classList.contains('dark-mode')) {
+      darkModeIcon.classList.add('fa-sun');
+      darkModeIcon.classList.remove('fa-moon');
+    } else {
+      darkModeIcon.classList.add('fa-moon');
+      darkModeIcon.classList.remove('fa-sun');
+    }
+    saveDarkModePreference();
+  });
+
 
   // Render all tasks on page load
   tasks.forEach(task => renderTask(task));
@@ -65,4 +92,9 @@ document.addEventListener('DOMContentLoaded', () => {
   function saveTasks() {
       localStorage.setItem("tasks", JSON.stringify(tasks));
   }
+
+  function saveDarkModePreference() {
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode));
+}
 });
